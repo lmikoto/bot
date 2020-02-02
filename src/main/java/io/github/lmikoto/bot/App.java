9c6928701ce8cb37a4ffd55143cb8b74be07aa2c
lmikoto <lmikoto@outlook.com>
2020-02-02 08:06:22 +0800

    fix: 纠正时区错误

--- src/main/java/io/github/lmikoto/bot/App.java
+++ src/main/java/io/github/lmikoto/bot/App.java
@@ -6,11 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)
 @EnableJpaRepositories(repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
 public class App {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
         SpringApplication.run(App.class,args);
     }
 }
